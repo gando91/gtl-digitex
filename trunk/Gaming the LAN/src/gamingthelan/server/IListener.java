1e6be5d50b3e0c226bc1c52b8db94b59104e9739
gandinialessio@gmail.com <gandinialessio@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-03-26 19:55:41 +0000

    Defined some features of the listener.It is not completed yet. Now it is a singleton.

--- trunk/Gaming the LAN/src/gamingthelan/server/IListener.java
+++ trunk/Gaming the LAN/src/gamingthelan/server/IListener.java
@@ -1,5 +1,11 @@
 package gamingthelan.server;
 
 public interface IListener {
-
+	
+	/**
+	 * Crea una nuova partita
+	 * @param porta La porta su cui mettere in ascolto il server
+	 */
+	public void creaPartita(int porta);
+	
 }
