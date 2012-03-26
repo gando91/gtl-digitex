eb729cf3a8bb751c79388f517a909aeb25b2a95e
gandinialessio@gmail.com <gandinialessio@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-03-26 19:55:41 +0000

    Defined some features of the listener.It is not completed yet. Now it is a singleton.

--- Gaming the LAN/src/gamingthelan/server/ServerListener.java
+++ Gaming the LAN/src/gamingthelan/server/ServerListener.java
@@ -1,7 +1,88 @@
 package gamingthelan.server;
 
-public class ServerListener implements IListener {
-	//TODO : Deve essere il thread di ascolto delle connessioni
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class ServerListener implements IListener, Runnable {
 	
 	private IServer mediatore;
+	private int porta = 8080;
+	private boolean listening;
+	
+	private ServerListener instance;
+	
+	private ServerListener() {
+
+	}
+	
+	public ServerListener getInstance() {
+		
+		if (instance == null) {
+			instance = new ServerListener();
+		}
+		
+		return instance;
+	}
+	
+	
+	public int getPorta() {
+		return porta;
+	}
+
+	public void setPorta(int porta) {
+		this.porta = porta;
+	}
+
+	@Override
+	public void creaPartita(int porta) {
+		mediatore = Server.getInstance();
+
+		Thread ascoltatore = new Thread(this);
+		ascoltatore.start();
+		listening = true;
+	}
+
+	@Override
+	public void run() {
+		
+		//FIXME : Istanziare una classe abstract (non zi pote, come fare per farlo fare al programmatore ?)
+		ServerSocket s = null;
+		
+		// Inizializzazione
+		try {
+			s = new ServerSocket(porta);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		while (listening) {
+			
+			try {
+				Socket socket = s.accept();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			/*
+			 * Bisogna creare un nuovo oggetto di tipo IConnection
+			 * e avviarlo in un nuovo thread
+			 * poi bisogna aggiunegere la nuova connessione al server
+			 * 
+			 * Il problema è che la nostra connessione è astratta... chi mi da una mano ragazzi ?
+			 * 
+			Client c = new  Client(socket, count, mediator);
+			Thread t = new Thread(c);
+			mediatore.addClient(c);
+			
+			t.start();
+			*/
+
+		}
+		
+	}
 }
