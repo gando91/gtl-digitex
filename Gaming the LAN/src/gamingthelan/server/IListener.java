7db874dcc041d7ed1c5386d0f794b32c2697dcf7
dagrada.marco@gmail.com <dagrada.marco@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-03-27 08:54:30 +0000

    Some name refactoring.

--- Gaming the LAN/src/gamingthelan/server/IListener.java
+++ Gaming the LAN/src/gamingthelan/server/IListener.java
@@ -6,6 +6,6 @@ public interface IListener {
 	 * Crea una nuova partita
 	 * @param porta La porta su cui mettere in ascolto il server
 	 */
-	public void creaPartita(int porta);
+	public void newGame(int port);
 	
 }
