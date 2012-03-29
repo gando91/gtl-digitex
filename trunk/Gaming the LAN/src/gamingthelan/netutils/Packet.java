fd615b49b5402124c624eb8eb7d6efc597c277d6
dagrada.marco@gmail.com <dagrada.marco@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-03-29 14:10:25 +0000

    Some imperfection.

--- trunk/Gaming the LAN/src/gamingthelan/netutils/Packet.java
+++ trunk/Gaming the LAN/src/gamingthelan/netutils/Packet.java
@@ -6,9 +6,10 @@ package gamingthelan.netutils;
  */
 public class Packet implements IPacket{
 	
-	//C'è da decidere come implementare mittente e destinatario del pacchetto, che tipo di attributi sono? 
-	//Esiste già un oggetto che contenga tali informazioni? E se è un messaggio di broadcast?
-	
+	// C'è da decidere come implementare mittente e destinatario del pacchetto, che tipo di attributi sono? 
+	// Esiste già un oggetto che contenga tali informazioni? E se è un messaggio di broadcast?
+	private Object sender;
+	private Object receiver;
 	
 
 }
