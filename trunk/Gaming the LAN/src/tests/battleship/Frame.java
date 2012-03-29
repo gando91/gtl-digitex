8a0603b68dae49fbf8b91376b11b818b29b92d04
mrmaiocchi91@gmail.com <mrmaiocchi91@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-03-29 09:11:54 +0000

    Source code repositioning due to code manager's epic fail

--- trunk/Gaming the LAN/src/tests/battleship/Frame.java
+++ trunk/Gaming the LAN/src/tests/battleship/Frame.java
@@ -4,19 +4,19 @@ import javax.swing.JFrame;
 
 public class Frame extends JFrame  {
 	
+	private static final long serialVersionUID = 1L;
+
 	public Frame(){
 		
 		setTitle("Battleship!");
 		setSize(1024,768);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
 		setResizable(false);
 		
-		
 		MainPanel mainpanel=new MainPanel();
-		
 		getContentPane().add(mainpanel);
 		
+		setVisible(true);
 	}
 
 	public static void main(String[] args) {
