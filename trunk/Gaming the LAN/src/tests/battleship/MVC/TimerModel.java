6e6953a6f02f40deebd079128bf3c8e4d6cf11e3
gandinialessio@gmail.com <gandinialessio@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-04-18 08:17:13 +0000

    A rough implementation of "TimerModel" is now available. Use this with an Observer.

---
+++ trunk/Gaming the LAN/src/tests/battleship/MVC/TimerModel.java
@@ -0,0 +1,61 @@
+package tests.battleship.MVC;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observable;
+
+import javax.swing.Timer;
+
+/**
+ * This class rapresent the model of a Battleship timer.
+ * It can be autoupdated. If you wanna monitor this, use an observer
+ * @author Alessio
+ *
+ */
+public class TimerModel extends Observable implements ActionListener{
+
+	//Costante limite di tempo
+	private static final int timeLimit = 40;
+	
+	private int timerCount;
+	private Timer myTimer;
+	
+	public TimerModel() {
+		super();
+		
+		//Inizializzo il tempo del timer
+		timerCount = timeLimit;
+		
+		myTimer = new Timer(1000, this);
+		
+	}
+	
+	public void startTimer() {
+		timerCount = timeLimit;
+		myTimer.start();
+	}
+
+	public void pauseTimer() {
+		myTimer.stop();
+	}
+	
+	public void resetTimer() {
+		timerCount = timeLimit;
+		myTimer.stop();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if (timerCount > 0)
+			timerCount--;
+		
+		if (timerCount == 0) {
+			//TODO : Devo avvisare qualcuno che il timer sia arrivato a 0
+		}
+		
+		this.setChanged();
+		this.notifyObservers();
+	}
+	
+}
