56cc9ad8df0bdd34cc0565c45d1b45700fbb3620
mrmaiocchi91@gmail.com <mrmaiocchi91@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-04-23 08:54:22 +0000

    Connection window updated

---
+++ trunk/Gaming the LAN/src/tests/connection/ConnectionPanel.java
@@ -0,0 +1,55 @@
+package tests.connection;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ConnectionPanel extends JPanel{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ConnectionPanel(){
+		JTextField nickname=new JTextField();
+		JTextField ip_server=new JTextField();
+		JTextField port=new JTextField();
+		
+		JButton connetti=new JButton("CONNECT");
+		JButton esci=new JButton("EXIT");
+		
+		port.setSize(10, 10);
+		
+		setBackground(Color.BLUE);
+		setLayout(new GridLayout(4,2));
+		
+		setBorder(BorderFactory.createEtchedBorder());
+		
+		JLabel nk=new JLabel("NICKNAME");
+		nk.setForeground(Color.WHITE);
+		add(nk);
+		add(nickname);
+		
+		JLabel is=new JLabel("IP SERVER");
+		is.setForeground(Color.WHITE);
+		add(is);
+		add(ip_server);
+		
+		JLabel p=new JLabel("PORT");
+		p.setForeground(Color.WHITE);
+		add(p);
+		add(port);
+		
+		add(connetti);
+		add(esci);
+		
+		
+		
+		setVisible(true);
+		
+	}
+
+}
