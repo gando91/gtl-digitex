10bcfcf9f92a5544990378df12c6bf44f0451e3e
dagrada.marco@gmail.com <dagrada.marco@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-04-24 08:53:45 +0000

    Now everything is working :-) Stream creation is not stucked, yet. Alla faccia tua !

--- trunk/Gaming the LAN/src/gamingthelan/netutils/IPacket.java
+++ trunk/Gaming the LAN/src/gamingthelan/netutils/IPacket.java
@@ -1,11 +1,12 @@
 package gamingthelan.netutils;
 
+import java.io.Serializable;
 import java.util.List;
 
 /*
  * Interfaccia che identifica un oggetto di tipo pacchetto
  */
-public interface IPacket {
+public interface IPacket extends Serializable {
 	
 	public List<IConnection> getReceiver();
 	
@@ -13,5 +14,4 @@ public interface IPacket {
 	
 	public void setSender(IConnection sender);
 	
-	public Object getContent();
 }
--- trunk/Gaming the LAN/src/miniTestClient/MyPacket.java
+++ trunk/Gaming the LAN/src/miniTestClient/MyPacket.java
@@ -3,18 +3,44 @@ package miniTestClient;
 import java.util.List;
 
 import gamingthelan.netutils.IConnection;
+import gamingthelan.netutils.IPacket;
 import gamingthelan.netutils.Packet;
 
-public class MyPacket extends Packet{
+public class MyPacket implements IPacket{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 5692030487608480118L;
 
-	public MyPacket(IConnection sender, List<IConnection> receiver, String s) {
-		super(sender, receiver,s);
-
+	private String testo;
+	private Integer a;
+	
+	@Override
+	public List<IConnection> getReceiver() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
+	@Override
+	public IConnection getSender() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setSender(IConnection sender) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setTesto(String testo, Integer a)
+	{
+		this.testo = testo;
+		this.a = a;
+	}
+	
+	public String getContent() {
+		return testo+a;
+	}
 }
