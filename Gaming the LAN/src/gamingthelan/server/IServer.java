c8ede7cb55c67beedf6e6c895c7bcc0c2014c5ab
simone91gv@gmail.com <simone91gv@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-04-27 15:13:48 +0000

    added the javadoc for the package Server in gamingthelan

--- Gaming the LAN/src/gamingthelan/server/IServer.java
+++ Gaming the LAN/src/gamingthelan/server/IServer.java
@@ -8,18 +8,60 @@ import gamingthelan.netutils.ConnectionHandler;
 import gamingthelan.netutils.IConnection;
 import gamingthelan.netutils.IPacket;
 
+/** This is the generic server interface
+ * **/
+
 
 public interface IServer {
 	
+	/**create a connection from a socket and a valid handler and then add it to Server's list 
+	 * 
+	 * @param socket socket connection initiation
+	 * @param handler handler which handles packets 
+	 *  
+	 *  **/
+	
 	public void createConnection(Socket socket, ConnectionHandler handler);
 	
+	/**add a just created connection to the server connection list
+	 * @param connessione target client connection 
+	 * 
+	 * **/
+	
 	public void addConnection(IConnection connessione);
+	
+	/**remove a connection from the Server's List
+	 * @param connessione target client connection 
+	 * 
+	 * **/
+	
 	public void rmConnection(IConnection connessione);
 	
+	/**Send a packet to every connected client
+	 *@param pacchetto packet to send to every connected client 
+	 *
+	 * **/
 	public void broadcastMessage(IPacket pacchetto) throws IOException;
+	
+	/**Send a packet to target connection
+	 * 
+	 * @param pacchetto packet to send to target connection
+	 * @param connessione target client connection
+	 * 
+	 * **/
 	public void sendMessage(IPacket pacchetto, IConnection connessione);
 	
+	/**add to the bannned list target address and close his connection
+	 * 
+	 * @param address internet address of target connection
+	 * 
+	 * **/ 
 	public boolean ban(InetAddress address);
+	
+	/**remove an address from the banned list
+	 * 
+	 * @param address internet address of target connection
+	 * **/
 	public boolean unban(InetAddress address);
 	
 	
