d78c75a7221f7a1c11d0c5474798a25f7168832a
mrmaiocchi91@gmail.com <mrmaiocchi91@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-05-18 09:11:18 +0000

    Splashscreen added

--- trunk/Gaming the LAN/src/tests/splashscreen/SplashPanel.java
+++ trunk/Gaming the LAN/src/tests/splashscreen/SplashPanel.java
@@ -1,6 +1,7 @@
 package tests.splashscreen;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import javax.swing.ImageIcon;
@@ -10,18 +11,33 @@ public class SplashPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private ImageIcon sfondo=new ImageIcon(getClass().getResource("BattleshipInitial.png"));
-	
+	private String play="PLAY";
+	private String info="HOW TO PLAY";
+	private String credits="CREDITS";
+	private String exit="EXIT";
 	
 	public SplashPanel(){
 		
 		setBackground(Color.BLACK);
 		setVisible(true);
-		
 	}
 	@Override
 	protected void paintComponent(Graphics arg0) {
 		
 		super.paintComponent(arg0);
 		arg0.drawImage(sfondo.getImage(),0,0,null);
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		
+		super.paint(g);
+		
+		g.setColor(Color.WHITE);
+		g.setFont(new Font("Homoarakhn", 0, 30));
+		g.drawString(play, 40, 100);
+		g.drawString(info, 40, 150);
+		g.drawString(credits, 40, 200);
+		g.drawString(exit, 40, 250);
 	}
 }
