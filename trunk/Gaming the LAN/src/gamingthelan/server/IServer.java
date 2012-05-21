04490a0bbc54c7a829f7eb387867a7feabf83c6e
gandinialessio@gmail.com <gandinialessio@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-05-21 10:01:39 +0000

    Removed a solved FIXME.

--- trunk/Gaming the LAN/src/gamingthelan/server/IServer.java
+++ trunk/Gaming the LAN/src/gamingthelan/server/IServer.java
@@ -49,7 +49,7 @@ public interface IServer {
 	 * @param connessione target client connection
 	 * 
 	 * **/
-	public void sendMessage(IPacket pacchetto); //FIXME : Praticamente inutilizzabile dal momento che la singola connection che dovrebbe usarlo nono ha nessun riferimento alla connessione target. Bisogna inventare qualcosa (un ID magari) per identificare una connessione senza tirarci dierteo tutto l'oggetto (che tra parentesi ha poco senso trasmettere, e non è serializzabile)
+	public void sendMessage(IPacket pacchetto);
 	
 	/**add to the bannned list target address and close his connection
 	 * 
