8fa6b2bff3a1bce26e31420381babc4727adfa55
mrmaiocchi91@gmail.com <mrmaiocchi91@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-05-22 08:58:45 +0000

    now first game test is working

--- trunk/Gaming the LAN/src/tests/connection/PacketHandler.java
+++ trunk/Gaming the LAN/src/tests/connection/PacketHandler.java
@@ -1,10 +1,15 @@
 package tests.connection;
 
-import gamingthelan.netutils.ConnectionHandler;
+import gamingthelan.client.ClientConnectionHandler;
+import gamingthelan.client.IClient;
 import gamingthelan.netutils.IPacket;
 import gamingthelan.netutils.ObjectPacket;
 
-public class PacketHandler implements ConnectionHandler{
+public class PacketHandler extends ClientConnectionHandler{
+
+	public PacketHandler(IClient client) {
+		super(client);
+	}
 
 	private ObjectPacket packet;
 
