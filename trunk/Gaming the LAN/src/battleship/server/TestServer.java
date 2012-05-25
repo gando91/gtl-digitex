7fed39194c220d0e70189e05cb29d8bb8296dbe0
gandinialessio@gmail.com <gandinialessio@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-05-25 17:34:39 +0000

    Now the server is a bit more verbose

--- trunk/Gaming the LAN/src/battleship/server/TestServer.java
+++ trunk/Gaming the LAN/src/battleship/server/TestServer.java
@@ -5,13 +5,16 @@ import gamingthelan.server.ServerListener;
 public class TestServer {
 
 	public static void main(String[] args) {
+		System.out.println(" --- BATTAGLIA NAVALE V 0.9 Beta ---");
+		System.out.println("          by Gruppo Digitex");
 		
 		//Istanziamo un nuovo ServerListener, che si occupa di gestire le richieste di connessione da parte dei client
 		ServerListener l = ServerListener.getInstance();
 		
+		System.out.println("Avvio del server listener sulla porta 8080 ...");
 		//Il metodo newGame mette il nostro ServerListener in "ascolto" di richieste di connessione in arrivo sulla porta specificata
 		l.newGame(8080, new ServerHandler());
-		
+		System.out.println("Server avviato correttamente");
 	}
 	
 }
