cb06f6ce38bb3b1edb4d2836adbd344d90f307f7
eric.villa91@gmail.com <eric.villa91@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-05-25 10:18:46 +0000

    Implemented a new design for the waiting window.

--- trunk/Gaming the LAN/src/battleship/positioning/WaitingWindow.java
+++ trunk/Gaming the LAN/src/battleship/positioning/WaitingWindow.java
@@ -1,34 +1,74 @@
 package battleship.positioning;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
+import java.util.LinkedList;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class WaitingWindow extends JFrame {
+public class WaitingWindow extends JFrame implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
+	
+	private static final int WIDTH = 200;
+	private static final int HEIGHT = 100;	
+	
+	private Timer timer;
+	private JPanel p;
+	private JLabel l;
+	
+	private LinkedList<String> dots = new LinkedList<String>();
+	private int index = 0;
 
 	public WaitingWindow() {
 		
-		setTitle("Please Wait ...");
-		setSize(300, 200);
+		setTitle("Please Wait");
+		setSize(WIDTH, HEIGHT);
+		setResizable(false);
 		
-		JPanel p = new JPanel();
-		p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS));
-		p.add(new JLabel("Waiting for the enemy..."));
+		dots.add(".");
+		dots.add("..");
+		dots.add("...");
+		dots.add("..");
 		
-		//TODO : Aggiungere GIF
+		l = new JLabel("   Waiting for the enemy");
+		l.setForeground(Color.WHITE);
 		
-		add(p);
+		p = new JPanel();
+		p.setBackground(Color.BLACK);			
+		p.setLayout(new GridLayout());
+		
+		p.add(l);
+		
+		add(p);		
 		setVisible(true);
+		
+		timer = new Timer(5,this);
+		timer.setDelay(1000);
+		timer.start();
 	}
 	
     public void closeFrame() {
         WindowEvent wev = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
         Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(wev);
     }
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+				
+		l.setText("   Waiting for the enemy" + dots.get(index));
+		index++;
+		
+		if(index==4)
+			index=0;
+	}
 }
