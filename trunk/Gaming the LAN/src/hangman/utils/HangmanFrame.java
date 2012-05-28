29d00be74ac1d19aa6ec139ac1f4c66a14a38df1
mrmaiocchi91@gmail.com <mrmaiocchi91@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-05-28 08:05:50 +0000

    HangmanPanel added

---
+++ trunk/Gaming the LAN/src/hangman/utils/HangmanFrame.java
@@ -0,0 +1,29 @@
+package hangman.utils;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class HangmanFrame extends JFrame {
+
+	private static final long serialVersionUID = 8409932111971291291L;
+	private static final int DEFAULT_HEIGHT = 150;
+	private static final int DEFAULT_WIDTH= 150;
+	
+	HangmanPanel panel=new HangmanPanel();
+	public HangmanFrame(){
+		
+		setTitle("Hangman!");
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		setResizable(false);
+	
+		add(panel);
+		
+		setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		HangmanFrame frame=new HangmanFrame();
+	}
+}
---
+++ trunk/Gaming the LAN/src/hangman/utils/HangmanPanel.java
@@ -0,0 +1,79 @@
+package hangman.utils;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class HangmanPanel extends JPanel{
+
+	private static final long serialVersionUID = -4235465505089593172L;
+	private int state=0; 
+	
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+
+	public HangmanPanel() {
+		
+		setBackground(Color.BLUE);
+		
+		setVisible(true);
+	}
+	
+	public void goOn()
+	{
+		state++;
+		repaint();
+	}
+	
+	public void reset()
+	{
+		state=0;
+		repaint();
+	}
+	
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		
+		if(state==1)
+			g.drawLine(20, 100, 100, 100);
+		
+		if(state==2)
+			g.drawLine(20, 100, 20, 20);
+			
+		if(state==3)
+			g.drawLine(20, 20, 70, 20);
+			
+		if(state==4)
+			g.drawLine(70, 20, 70, 30);
+			
+		if(state==5)
+			g.drawOval(63, 30, 15, 15);
+		
+		if(state==6)
+			g.drawLine(70, 45, 70, 75);
+			
+		if(state==7)
+			g.drawLine(70, 47, 80, 57);
+		
+		if(state==8)
+			g.drawLine(70, 47, 60, 57);
+		
+		if(state==9)
+			g.drawLine(70, 75, 80, 85);
+		
+		if(state==10)
+			g.drawLine(70, 75, 60, 85);
+			
+	}
+	
+
+}
