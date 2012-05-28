b8b59843667f2d1ea285c6e78bf234a5993682ac
eric.villa91@gmail.com <eric.villa91@gmail.com@059ebd08-5493-983c-61e1-940be5c34f7b>
2012-05-28 20:05:38 +0000

    Now the WaitingWindow will close when game starts.

--- trunk/Gaming the LAN/src/hangman/client/PacketHandler.java
+++ trunk/Gaming the LAN/src/hangman/client/PacketHandler.java
@@ -8,6 +8,7 @@ import gamingthelan.client.ClientConnectionHandler;
 import gamingthelan.client.IClient;
 import gamingthelan.netutils.IPacket;
 import gamingthelan.netutils.servicepackets.DisconnectionPacket;
+import hangman.connection.WaitingWindow;
 import hangman.packets.ProtocolPacket;
 import hangman.packets.WordPacket;
 import hangman.packets.WrongPacket;
@@ -16,40 +17,36 @@ public class PacketHandler extends ClientConnectionHandler{
 	
 	private int state = 0;
 	private MainPanel panel;
+	private WaitingWindow waitingwindow;
 	
 	public PacketHandler(IClient client) {
-		super(client);
-		
+		super(client);		
 	}
 
 	@Override
-	public void onReceivedPacket(IPacket packet) {
+	public void onReceivedPacket(IPacket packet) {		
 		
-		
-		if (state == 0)
-		{
-			if ( ((ProtocolPacket)packet).getCode() == 0 ){
+		if (state == 0){			
+			if ( ((ProtocolPacket)packet).getCode() == 0 ){				
 				ProtocolPacket p = new ProtocolPacket(this.client.getConnection().getNickName(), null, 500);
+				
 				try {
 					this.client.sendPacket(p);
 					state = 1;
-					System.out.println("AAAAAA");
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				
-				
-			}
+			}			
 		}
 		else if (state == 1) {
 			
-			if(packet instanceof ProtocolPacket){
-				
+			if(packet instanceof ProtocolPacket){				
 				if ( ((ProtocolPacket)packet).getCode() == 1000 ){
-					panel = new MainPanel(null, this.client);
-					System.out.println("ciaooooo");
 					
+					closeWaitingWindow();
+					panel = new MainPanel(null, this.client);
 				}
 			}
 			
@@ -59,19 +56,23 @@ public class PacketHandler extends ClientConnectionHandler{
 				state = 2;
 			}
 		}		
-		 else if (state == 2) {
+		else if (state == 2) {
+			
 			if(packet instanceof WordPacket){
 				panel.setWord(((WordPacket)packet).getWord());
+				
 				if(packet.getSender() != null){
 					JOptionPane.showMessageDialog(null, packet.getSender()+" ha indovinato la parola !");
 					panel.setEnabled(false);
 				}
 			}
+			
 			if(packet instanceof WrongPacket){
 				panel.hangUp(((WrongPacket)packet).getStatus());
 				JOptionPane.showMessageDialog(null, "La lettera '"+((WrongPacket)packet).getLetter() +"' inserita da " + ((WrongPacket)packet).getDelinquent() + " non è presente");
 				panel.setEnabled(false);
 			}
+			
 			if(packet instanceof ProtocolPacket){
 				if(((ProtocolPacket)packet).getCode() == 1500){
 					panel.setEnabled(true);
@@ -79,12 +80,20 @@ public class PacketHandler extends ClientConnectionHandler{
 			}
 			
 		}
-}
+	}
 	
 
 	@Override
 	public void onDisconnectedClient(DisconnectionPacket packet) {
 		// TODO Auto-generated method stub		
+	}
+	
+	public void openWaitingWindow(){
+		this.waitingwindow = new WaitingWindow();
+	}
+	
+	private void closeWaitingWindow(){
+		this.waitingwindow.closeFrame();
 	}
 
 }
--- trunk/Gaming the LAN/src/hangman/server/ServerHandler.java
+++ trunk/Gaming the LAN/src/hangman/server/ServerHandler.java
@@ -21,18 +21,20 @@ public class ServerHandler implements ConnectionHandler {
 	public static final int MAXPLAYERS = 2;
 	public static final int HANGMANDEATH = 10;
 	
-	private HashMap<String, Boolean> players = new HashMap<String, Boolean>();
-	private int nready = 0;
+	private HashMap<String, Boolean> players = new HashMap<String, Boolean>();	
 	private String[] player = new String[MAXPLAYERS];
-	private ProtocolPacket p;
-	private List<String> parole = new LinkedList<String>();
+	private int nready = 0;
+	private int turn = 0;	
 	private Words words;
-	private int turn = 0;
+	private List<String> parole = new LinkedList<String>();
+	private ProtocolPacket p;
+	
 	private int hangmanstatus;
 	
 	/*Implementiamo qui il metodo onReceivedPacket della classe ConnectionHandler, scegliendo come deve comportarsi il nostro 
 	 * server nel momento in cui arriva un pacchetto */
 	public ServerHandler(){
+		
 		parole.add("dagrada");
 		parole.add("maiocchi");
 		parole.add("villa");
@@ -42,58 +44,66 @@ public class ServerHandler implements ConnectionHandler {
 		parole.add("maiocchiculo");
 		
 		words = new Words(parole);
+		
 		CurrentWord.getInstance().setCurrentWord(words.getRndWord());
 	}
 	
 	@Override
-	public void onReceivedPacket(IPacket packet) {
-		
+	public void onReceivedPacket(IPacket packet) {		
 		
 		if(packet instanceof ProtocolPacket){
 		
 			if ( ((ProtocolPacket)packet).getCode() == 0 ){
+				
 				ProtocolPacket p = new ProtocolPacket(null, packet.getSender(), 0);
 				
 				//Se il giocatore non c'è
 				if ( !players.containsKey(packet.getSender()))
 					players.put(packet.getSender(), Boolean.FALSE);
 				
-				Server.getInstance().sendMessage(p);
-				
+				Server.getInstance().sendMessage(p);				
 			}
 			
 			if(((ProtocolPacket)packet).getCode() == 500){
 				
 				System.out.println(packet.getSender() + " è pronto");
-				if(players.containsKey(packet.getSender()))
-				{
+				
+				if(players.containsKey(packet.getSender())){
+					
 					players.put(packet.getSender(), Boolean.TRUE);
 					player[nready]=packet.getSender();
 					nready++;
 				}
 				
-				if (nready == MAXPLAYERS)
-				{
+				if (nready == MAXPLAYERS){
 					
 					ProtocolPacket p = new ProtocolPacket(null, null, 1000);
+					
 					try {
+						// Dico a tutti che abbiamo raggiunto il tetto massimo di giocatori pronti
 						Server.getInstance().broadcastMessage(p);
-						System.out.println("CACCA");
+						
+						// Dico al primo giocatore che è il suo turno.
 						ProtocolPacket q = new ProtocolPacket(null, player[turn], 1500);
-						Server.getInstance().sendMessage(q);
-						System.out.println(player[turn]);
+						Server.getInstance().sendMessage(q);	
+						
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
+					
 					WordPacket w = new WordPacket(null, null, CurrentWord.getInstance().getVisibleString());
+					
 					try {
+						// Mando a tutti la parola da indovinare.
 						Server.getInstance().broadcastMessage(w);
-						System.out.println(CurrentWord.getInstance().getVisibleString());
+						
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
+					
 					System.out.println("Partita Avviata !");
+					
 					nready = 0;
 				}				
 			}			
@@ -103,8 +113,10 @@ public class ServerHandler implements ConnectionHandler {
 			
 			int answer = CurrentWord.getInstance().letterCheck(((LetterPacket)packet).getLetter());			
 			
-			if(answer == 0){
+			// Se la lettera non è corretta.
+			if(answer == 0){				
 				hangmanstatus++;
+				
 				if(hangmanstatus != HANGMANDEATH){
 					WrongPacket a = new WrongPacket(packet.getSender(), ((LetterPacket)packet).getLetter(), hangmanstatus);
 					
@@ -115,10 +127,17 @@ public class ServerHandler implements ConnectionHandler {
 						e.printStackTrace();
 					}
 				}
+				
+				else{
+					
+					// TODO: dobbiamo dire ai client che la partita è terminata.
+				}
 			}			
 			
+			// Se la lettera è corretta ma non ha vinto nessuno.
 			if(answer == 1){
 				WordPacket wp = new WordPacket(null, null, CurrentWord.getInstance().getVisibleString());
+				
 				try {
 					Server.getInstance().broadcastMessage(wp);
 					
@@ -128,8 +147,10 @@ public class ServerHandler implements ConnectionHandler {
 				}
 			}			
 			
+			// Se la lettera è corretta e ha vinto il giocatore di turno.
 			if(answer == 2){
 				WordPacket wp = new WordPacket(player[turn], null, CurrentWord.getInstance().getVisibleString());
+				
 				try {
 					Server.getInstance().broadcastMessage(wp);
 					
@@ -142,14 +163,14 @@ public class ServerHandler implements ConnectionHandler {
 			turnGoOn();
 		}
 		
+		// Se un client mi ha mandato un pacchetto "I FEEL LUCKY" che è effettivamente un WordPacket
 		if(packet instanceof WordPacket){
 			
 			WordPacket wp;
 			
-			if(CurrentWord.getInstance().getWord() == ((WordPacket)packet).getWord()){
-				wp = new WordPacket(player[turn], null, CurrentWord.getInstance().getWord());
-				
-			}
+			// Se la parola corrente è uguale a quella inviata dal giocatore di turno
+			if(CurrentWord.getInstance().getWord() == ((WordPacket)packet).getWord())
+				wp = new WordPacket(player[turn], null, CurrentWord.getInstance().getWord());				
 			else
 				wp = new WordPacket(null, null, CurrentWord.getInstance().getWord());
 			
@@ -172,9 +193,9 @@ public class ServerHandler implements ConnectionHandler {
 	}
 	
 	public void turnGoOn(){
-		if(turn == MAXPLAYERS){
+		
+		if(turn == MAXPLAYERS)
 			turn++;
-		}
 		else
 			turn = 0;
 		
